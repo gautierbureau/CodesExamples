468f19b6a94c50aad3458f391fcd3ebb320de4eb
Gautier Bureau <gautier.bureau@gmail.com>
2019-04-03 21:23:25 +0200

    added ref for autoconf

--- autoconf/launch.sh
+++ autoconf/launch.sh
@@ -5,3 +5,13 @@ autoconf # Generate configure from configure.ac
 automake --add-missing # Generate Makefile.in from Makefile.am
 ./configure CC=clang # Generate Makefile from Makefile.in
 CC=clang make distcheck # Use Makefile to build and test a tarball to distribute
+
+#https://thoughtbot.com/blog/the-magic-behind-configure-make-make-install
+#https://ftp.gnu.org/old-gnu/Manuals/autoconf-2.57/html_chapter/autoconf_13.html
+#https://elinux.org/images/4/43/Petazzoni.pdf
+
+# http://www-igm.univ-mlv.fr/~dr/XPOSE/Breugnot/
+
+# http://www.idryman.org/blog/2016/03/10/autoconf-tutorial-1/
+# http://www-igm.univ-mlv.fr/~dr/XPOSE/Breugnot/
+# https://www.lrde.epita.fr/~adl/dl/autotools.pdf
